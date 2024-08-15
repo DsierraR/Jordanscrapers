316f2161fc9dca7449a86de3e58385259cd2bd42
DsierraR <dasierra@cougarnet.uh.edu>
2024-08-15 03:17:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -289,7 +289,7 @@ def create_visualizations(excel_buffer):
 def send_email(excel_buffer, changes):
     logging.info("Preparing to send email...")
     sender_email = "dsierraramirez115@gmail.com"
-    receiver_email = ["diegosierra01@yahoo.com".
+    receiver_email = ["diegosierra01@yahoo.com",
                       "arnav.ashruchi@gmail.com"]
         
     password = password = os.environ['EMAIL_PASSWORD']  # App-specific password for Gmail
